ce93860336b1921fe20dbe27c56a597ab2c044e1
Sung Noh <noh.sung@gmail.com>
2024-05-09 17:33:25 -0700

    Only set Tailscale alias on macOS
    Tailscale is installed through the app store i.e. installed as an Mac
    application. This isn't the case for all operating systems so we need to
    place a guard around the alias to avoid overwriting the CLI command on
    other OSes.

--- zsh/.zsh/aliases/misc.sh
+++ zsh/.zsh/aliases/misc.sh
@@ -2,4 +2,6 @@ pg() {
   pg_ctl -D /usr/local/var/postgres $1 -l /tmp/postgres.log
 }
 
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+case "$OSTYPE" in
+  darwin*) alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+esac
