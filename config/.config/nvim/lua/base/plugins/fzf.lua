3c597784bdd4c7eded5f4a3471b0400de1c3fd0a
Sung Noh <noh.sung@gmail.com>
2026-06-17 23:52:30 +0000

    Set `FZF_DEFAULT_COMMAND` with FZF in nvim

--- config/.config/nvim/lua/base/plugins/fzf.lua
+++ config/.config/nvim/lua/base/plugins/fzf.lua
@@ -4,6 +4,7 @@ return {
   dependencies = { "junegunn/fzf" },
 
   config = function()
+    vim.env.FZF_DEFAULT_COMMAND = "rg --files --hidden --glob '!.git'"
     vim.g.fzf_layout = { down = '40%' }
     vim.keymap.set("n", "<leader>e", ":FZF<cr>", { noremap = true })
   end
